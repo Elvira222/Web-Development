e6b3f0da359a685855a0960058a5e813cc85f7e4
Elvira Zhakupova <90088029+Elvira222@users.noreply.github.com>
2023-03-25 23:16:48 +0600

    Create main.py

--- lab-7/informatics/d/main.py
+++ lab-7/informatics/d/main.py
@@ -1,166 +1,60 @@
 import math
 
-# FOR
-
 # a
-# a = int(input())
-# b = int(input())
-# for i in range(a, b+1):
+# n = int(input())
+# a = list(input().split(' '))
+# for i in range(0, n):
 #     if i%2==0:
-#         print(i)
+#         print(a[i], end=' ')
 
 
 # b
-# a = int(input())
-# b = int(input())
-# c = int(input())
-# d = int(input())
-# for i in range(a, b+1):
-#     if i%d ==c:
-#         print(i)
+# n = int(input())
+# a = list(input().split(' '))
+# for i in range(0, n):
+#     if a[i]%2==0:
+#         print(a[i], end=' ')
 
 
 # c
-# a = int(input())
-# b = int(input())
-# for i in range(a, b + 1):
-# 	x = int(math.sqrt(i))
-# 	if x * x == i: print(i, end = ' ')
+# n = int(input())
+# a = [int(x) for x in input().split()]
+# cnt = 0
+# for i in a:
+# 	if i > 0: cnt += 1
+# print(cnt)
 
 
 # d
-# x = int(input())
-# d = int(input())
-# print(str(x).count(str(d)))
+# n = int(input())
+# a = [int(x) for x in input().split()]
+# cnt = 0
+# for i in range(1, n):
+# 	if a[i] > a[i - 1]: cnt += 1
+# print(cnt)
 
 
 # e
-# x = int(input())
-# summa = 0
-# while x > 0:
-#     temp = x%10
-#     summa += temp
-#     x //= 10
-# print(summa)
+# n = int(input())
+# a = [int(x) for x in input().split()]
+# for i in range(1, n):
+#     if a[i] * a[i - 1] > 0:
+#         print('YES')
+#         exit()
+# print('NO')
 
 
 # f
-# x = int(input())
-# summa = 0
-# while x>0:
-#     temp = x%10
-#     x //= 10
-#     summa *= 10
-#     summa += temp
-#
-# print(summa)
+# n = int(input())
+# a = [int(x) for x in input().split()]
+# cnt = 0
+# for i in range(1, n - 1):
+#     if a[i] > a[i - 1] and a[i] > a[i + 1]: cnt += 1
+# print(cnt)
 
 
 # g
-# x = int(input())
-# for i in range(2, x + 1):
-# 	if x % i == 0:
-# 		print(i)
-# 		break
-
-
-# h
-# x = int(input())
-# for i in range(1, x + 1):
-# 	if x % i == 0:
-# 		print(i, end = ' ')
-
-
-# i
-# n = int(input())
-# x = int(math.sqrt(n))
-# cnt = 0
-# for i in range(1, x):
-#     if n % i == 0: cnt += 2
-# if n % x == 0: cnt += 1
-# print(cnt)
-
-
-# j
-# cnt = 0
-# for i in range(100):
-#     cnt += int(input())
-# print(cnt)
-
-
-# k
-# n = int(input())
-# summa = 0
-# for i in range(0, n):
-#     x = int(input())
-#     summa += x
-# print(summa)
-
-
-# l
-# print(int(input(), 2))
-
-
-# m
-# n = int(input())
-# cnt = 0
-# for i in range(0, n):
-#     x = int(input())
-#     if x == 0:
-#         cnt += 1
-# print(cnt)
-
-
-
-
-#  WHILE
-
-# a
-# n = int(input())
-# i = 1
-# while n > 0 and i <= n:
-#     x = int(math.sqrt(i))
-#     if x * x == i:
-#         print(i)
-#     i += 1
-
-
-# b
-# n = int(input())
-# i = 2
-# while True:
-#     if n % i == 0:
-#         print(i)
-#         break
-#     i += 1
-
-
-# c
-# def check(x):
-#     while x % 2 == 0:
-#         x //= 2
-#     return x == 1
-#
-# n = int(input())
-#
-# for i in range(1, n + 1):
-#     if check(i):
-#         print(i, end = ' ')
-
-
-# d
-# def check(x):
-#     while x % 2 == 0:
-#         x //= 2
-#     return x == 1
-#
-# n = int(input())
-# if check(n):
-#     print('YES')
-# else:
-#     print('NO')
-
-
-# e
-x = int(input())
-print(math.ceil(math.log2(x)))
+n = int(input())
+a = [int(x) for x in input().split()]
+for i in range(n-1, -1, -1):
+	print(a[i], end=' ')
